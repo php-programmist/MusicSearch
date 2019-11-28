f3addfbbfda54182ff05932c35b571aaff494cb0
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-11-28 19:34:49 +0300

    init

---
+++ app/src/main/java/ru/phpprogrammist/music/screens/search/SearchActivity.kt
@@ -0,0 +1,13 @@
+package ru.phpprogrammist.music.screens.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.phpprogrammist.music.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
