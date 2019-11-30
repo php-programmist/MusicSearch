a9368204f8508747b891578ef48b3ff51cb3f541
PHP-Programmist.ru <php.programmist.ru@gmail.com>
2019-12-01 00:19:24 +0300

    Сделал загрузку изображения альбома

--- app/src/main/java/ru/phpprogrammist/music/adapters/AlbumsPagedListAdapter.kt
+++ app/src/main/java/ru/phpprogrammist/music/adapters/AlbumsPagedListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.album_item.view.*
 import ru.phpprogrammist.music.R
 import ru.phpprogrammist.music.data.albums.Album
@@ -31,6 +32,10 @@ class AlbumsPagedListAdapter : PagedListAdapter<Album, AlbumsPagedListAdapter.Vi
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(item: Album?) = with(itemView) {
             collectionName.text = item?.collectionName?:"Error"
+            val url = item?.artworkUrl100
+            if(url!=null && url.isNotEmpty()){
+                Picasso.get().load(url).placeholder(R.drawable.no_image_placeholder).error(R.drawable.no_image_placeholder).into(albumPoster)
+            }
         }
     }
 }
